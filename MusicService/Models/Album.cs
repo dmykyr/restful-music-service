4870e9a412afde07785eca40dcd9d53e02c7ab16
kropli <dimakirimail@gmail.com>
2023-12-03 15:02:26 +0200

    Created classes for db entities

---
+++ MusicService/Models/Album.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicService.Models
+{
+    public class Album
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public Guid ArtistId { get; set; }
+
+        [Required]
+        public Guid SongId { get; set; }
+
+        [ForeignKey("ArtistId")]
+        public Artist Artist { get; set; }
+
+        [ForeignKey("SongId")]
+        public Song Song { get; set; }
+    }
+}
---
+++ MusicService/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicService.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid AccountId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Nickname { get; set; }
+
+        [ForeignKey("AccountId")]
+        public Account Account { get; set; }
+    }
+}
