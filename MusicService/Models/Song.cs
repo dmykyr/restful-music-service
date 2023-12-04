08e0ab97387e4cfe45f8a930ff8030b26aeb14ca
kropli <dimakirimail@gmail.com>
2023-12-04 17:18:53 +0200

    created entity classes based on database entity relationship model

---
+++ MusicService/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicService.Models
+{
+    public class Song
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public byte[] Track { get; set; }
+
+        [Required]
+        public DateTime PublishingDate { get; set; }
+    }
+}
