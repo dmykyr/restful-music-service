3088eafd35db47b7194593bfafcdb6cda551d1dd
Dmytro Kyrychenko <87243401+dmykyr@users.noreply.github.com>
2023-12-04 18:07:28 +0200

    Сreated entity models based on database entity relationship model

---
+++ MusicService/Models/Album.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicService.Models
+{
+    public class Album
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid PublisherId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public DateTime PublishingDate { get; set; }
+
+        [ForeignKey("PublisherId")]
+        public Artist Publisher { get; set; }
+    }
+}
---
+++ MusicService/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicService.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid RoleId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [Index(IsUnique = true)]
+        public string Nickname { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Password { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
+    }
+}
