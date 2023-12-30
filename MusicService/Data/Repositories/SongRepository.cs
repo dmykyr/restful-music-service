eb1f77ec88b800ba06221054f2a1b5aff958a178
kropli <dimakirimail@gmail.com>
2023-12-30 21:50:05 +0200

    Implemented song repository - Implemented base CRUD methods in SongRepository class - Added SongRepository as a scoped service in Program.cs

---
+++ MusicService/Data/Repositories/SongRepository.cs
@@ -0,0 +1,56 @@
+﻿using MusicService.Models;
+using System.Data.Entity;
+
+namespace MusicService.Data.Repositories
+{
+    public class SongRepository : IRepository<Song>
+    {
+        private readonly MusicDbContext _context;
+
+        public SongRepository(MusicDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Song>> GetAll()
+        {
+            return await _context.Songs.ToListAsync();
+        }
+
+        public async Task<Song> Get(Guid id)
+        {
+            return await _context.Songs.FindAsync(id);
+        }
+
+        public async Task<Song> Add(Song entity)
+        {
+            await _context.AddAsync(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<Song> Update(Song entity)
+        {
+            _context.Songs.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return entity;
+        }
+        
+        public async Task<Song> Delete(Guid id)
+        {
+            var song = await _context.Songs.FindAsync(id);
+
+            if (song != null)
+            {
+                _context.Songs.Remove(song);
+                await _context.SaveChangesAsync();
+
+                return song;
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
