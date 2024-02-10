bd587c2abf39061b21218b621a0748e96e2c12be
kropli <dimakirimail@gmail.com>
2024-02-10 13:36:29 +0200

    Add user controller
    - created UserController class
    - implemented endpoint for geting user info
    - implemented endpoints for create get and delete favorite album
    - implemented endpoints for create get and delete favorite artist
    - marked UserService as injectable in Program.cs file

--- MusicService/Data/Repositories/UserRepository.cs
+++ MusicService/Data/Repositories/UserRepository.cs
@@ -52,5 +52,49 @@ namespace MusicService.Data.Repositories
 
             throw new NotImplementedException();
         }
+
+        public async Task<IEnumerable<Album>> GetFavoriteAlbums(Guid userId, string searchName)
+        {
+            return await _context.FavoriteAlbums
+                .Include(fa => fa.Album)
+                .Where(fa => fa.UserId == userId && fa.Album.Title.Contains(searchName))
+                .Select(fa => fa.Album)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Artist>> GetFavoriteArtists(Guid userId, string searchName)
+        {
+            return await _context.FavoriteArtists
+                .Include(fa => fa.Artist)
+                .Where(fa => fa.UserId == userId && fa.Artist.Name.Contains(searchName))
+                .Select(fa => fa.Artist)
+                .ToListAsync();
+        }
+
+        public async Task AddFavoriteAlbum (Guid userId, Guid albumId)
+        {
+            await _context.FavoriteAlbums.AddAsync(new FavoriteAlbum { UserId = userId, AlbumId = albumId });
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddFavoriteArtist(Guid userId, Guid artistId)
+        {
+            await _context.FavoriteArtists.AddAsync(new FavoriteArtist { UserId = userId, ArtistId = artistId });
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveFavoriteAlbum(Guid userId, Guid albumId)
+        {
+            var favoriteAlbum = await _context.FavoriteAlbums.FirstAsync(fa => fa.UserId == userId && fa.AlbumId == albumId);
+            _context.FavoriteAlbums.Remove(favoriteAlbum);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task RemoveFavoriteArtist(Guid userId, Guid artistId)
+        {
+            var favoriteArtist = await _context.FavoriteArtists.FirstAsync(fa => fa.UserId == userId && fa.ArtistId == artistId);
+            _context.FavoriteArtists.Remove(favoriteArtist);
+            await _context.SaveChangesAsync();
+        }
     }
 }
